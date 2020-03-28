51f0ba3675caf23a7e4b56f564228b2aa1355bb6
HandiPutraTjioe <handibncc23@gmail.com>
2020-03-28 22:45:02 +0700

    Tugas 4 ReactJs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import MenuMakanan from "./Page/MenuMakanan";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <MenuMakanan />
+      </div>
+    );
+  }
+}
+
+export default App;
